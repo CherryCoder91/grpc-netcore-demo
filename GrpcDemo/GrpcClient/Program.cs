35467c8e398d39fc199b03476277d0e1ad59c9b8
James Pickup <jamespickup2@gmail.com>
2020-03-19 11:39:05 +0000

    Add server and client projects as barebone implementation.

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        private const string GRPC_ADDRESS = "https://localhost:5001";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press enter to make gRPC Request");
+            Console.ReadLine();
+
+            var channel = GrpcChannel.ForAddress(GRPC_ADDRESS);
+            var client = new Greeter.GreeterClient(channel);
+
+            HelloRequest request = new HelloRequest();
+            var response = await client.SayHelloAsync(request);
+
+            Console.WriteLine($"Message: {response.Message}");
+
+            Console.ReadLine();
+        }
+    }
+}

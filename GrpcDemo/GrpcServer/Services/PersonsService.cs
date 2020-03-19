3c1b80c8323144102a590231f990105748c049b2
James Pickup <jamespickup2@gmail.com>
2020-03-19 12:12:36 +0000

    Add Person Proto and Service including gRPC route mapping

---
+++ GrpcDemo/GrpcServer/Services/PersonsService.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class PersonsService : Persons.PersonsBase
+    {
+        private readonly ILogger<PersonsService> _logger;
+
+        public PersonsService(ILogger<PersonsService> logger)
+        {
+            this._logger = logger;
+        }
+
+        public override Task<PersonResponse> GetPerson(PersonRequest request, ServerCallContext context)
+        {
+            PersonResponse response = new PersonResponse();
+
+            if (request.UserId == 1)
+            {
+                response.UserId = 1;
+                response.FirstName = "James";
+                response.LastName = "Pickup";
+                response.IsVerified = true;
+            } else if (request.UserId == 2)
+            {
+                response.UserId = 2;
+                response.FirstName = "Cherry";
+                response.LastName = "Coder91";
+                response.IsVerified = true;
+            } else if (request.UserId == 3)
+            {
+                response.UserId = 3;
+                response.FirstName = "Mr";
+                response.LastName = "Developer";
+                response.IsVerified = false;
+            }
+
+
+            return Task.FromResult(response);
+        }
+    }
+}
--- GrpcDemo/GrpcServer/Startup.cs
+++ GrpcDemo/GrpcServer/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GrpcServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +29,7 @@ namespace GrpcServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<PersonsService>();
 
                 endpoints.MapGet("/", async context =>
                 {

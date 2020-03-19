634b3a4d8f0f5b99b16778f0f0a98e4c38807c15
James Pickup <jamespickup2@gmail.com>
2020-03-19 12:53:22 +0000

    Add streaming  of response objects example to both server and client

--- GrpcDemo/GrpcClient/Program.cs
+++ GrpcDemo/GrpcClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Grpc.Net.Client;
 using GrpcServer;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GrpcClient
@@ -10,6 +11,42 @@ namespace GrpcClient
         private const string GRPC_ADDRESS = "https://localhost:5001";
 
         static async Task Main(string[] args)
+        {
+            await DemonstrateStreaming();
+            //await DemonstrateSingleRequest();
+        }
+
+        /// <summary>
+        ///     Demonstrates a gRPC call that streams in multiple responses.
+        /// </summary>
+        /// <returns>Void Task</returns>
+        private async static Task DemonstrateStreaming()
+        {
+
+            Console.WriteLine("Press enter to make gRPC Request.");
+            Console.ReadLine();
+
+            var channel = GrpcChannel.ForAddress(GRPC_ADDRESS);
+            var client = new Persons.PersonsClient(channel);
+
+            using (var clientRequest = client.GetPersons(new PersonsRequest()))
+            {
+                CancellationTokenSource source = new CancellationTokenSource();
+                while (await clientRequest.ResponseStream.MoveNext(source.Token))
+                {
+                    var person = clientRequest.ResponseStream.Current;
+                    Console.WriteLine($"Firstname: {person.FirstName} Lastname: {person.LastName} Verified: {person.IsVerified}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        ///     Demonstrates a gRPC call that makes a request for a singular item
+        /// </summary>
+        /// <returns>Void Task</returns>
+        private async static Task DemonstrateSingleRequest()
         {
             Console.WriteLine("Press enter to make gRPC Request.");
             Console.ReadLine();
--- GrpcDemo/GrpcServer/Services/PersonsService.cs
+++ GrpcDemo/GrpcServer/Services/PersonsService.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace GrpcServer.Services
@@ -23,13 +24,15 @@ namespace GrpcServer.Services
                 response.FirstName = "James";
                 response.LastName = "Pickup";
                 response.IsVerified = true;
-            } else if (request.UserId == 2)
+            }
+            else if (request.UserId == 2)
             {
                 response.UserId = 2;
                 response.FirstName = "Cherry";
                 response.LastName = "Coder91";
                 response.IsVerified = true;
-            } else if (request.UserId == 3)
+            }
+            else if (request.UserId == 3)
             {
                 response.UserId = 3;
                 response.FirstName = "Mr";
@@ -39,6 +42,41 @@ namespace GrpcServer.Services
 
 
             return Task.FromResult(response);
+        }
+
+        public override async Task GetPersons(PersonsRequest request, IServerStreamWriter<PersonResponse> responseStream, ServerCallContext context)
+        {
+            List<PersonResponse> persons = new List<PersonResponse>
+            {
+                new PersonResponse
+                {
+                    UserId = 1,
+                    FirstName = "James",
+                    LastName = "Pickup",
+                    IsVerified = true
+                },
+                new PersonResponse
+                {
+                    UserId = 2,
+                    FirstName = "Cherry",
+                    LastName = "Coder91",
+                    IsVerified = true
+                },
+                new PersonResponse
+                {
+                    UserId = 3,
+                    FirstName = "Mr",
+                    LastName = "Developer",
+                    IsVerified = false
+                }
+            };
+
+            foreach (var person in persons)
+            {
+                await responseStream.WriteAsync(person);
+                await Task.Delay(2000); // Artiftical delay to demonstrate streaming more clearly in the client. Not a production concept!
+            }
+
         }
     }
 }
